f9ea9ce25a7a792e526c8fc7b034d24ce4e710fd
Adam <Adamqplett@gmail.com>
2023-11-06 21:36:51 -0600

    added basic movement to slime

--- Assets/Scripts/Enemy/Slime.cs
+++ Assets/Scripts/Enemy/Slime.cs
@@ -5,15 +5,44 @@ using UnityEngine;
 public class Slime : Enemy
 {
     public GameObject slimeSmaller;
-    public float spawnOffset=1;
+    [SerializeField] float spawnOffset=1, timeMoving=1.25f, timeStill=.5f;
+
+    private bool isMoving = false;
     // Update is called once per frame
+    public override void Awake()
+    {
+        setMovingFalse();
+        base.Awake();
+    }
     void Update()
     {
-        
+        Move();
     }
     public override void Move()
     {
-        throw new System.NotImplementedException();
+        if (isMoving == true)
+        {
+            //makes a vector in the direction of the player and sets its magnitiude to 1
+            Vector2 direction = player.transform.position - transform.position;
+            direction.Normalize();
+            //flips sprite in the direction of movement
+            if (direction.x < 0) transform.localScale = new Vector3(1, 1, 1);
+            if (direction.x > 0) transform.localScale = new Vector3(-1, 1, 1);
+            //turns the direction into an angle
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            //moves towards the player
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+    }
+    private void setMovingFalse()
+    {
+        isMoving = false;
+        Invoke(nameof(setMovingTrue), timeStill);
+    }
+    private void setMovingTrue()
+    {
+        isMoving = true;
+        Invoke(nameof(setMovingFalse), timeMoving);
     }
     public override void Death()
     {
