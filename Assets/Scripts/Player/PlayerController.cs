75cdd44a56ee684b5efd62ff35cfc9abb7b3e781
Adam <Adamqplett@gmail.com>
2023-11-10 14:33:06 -0600

    BOOSTER BE BOOSTIN' (fixed all booster bugs)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -31,6 +31,7 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         ActivateBooster(false);
+        playerForward = gameObject.transform.up;
     }
 
     private void FixedUpdate()
